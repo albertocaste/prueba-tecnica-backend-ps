41dfceb6bd12aee466a5b98faef7248bbda32f08
Alberto Castellanos Martínez <caste_acm@hotmail.com>
2024-01-23 03:15:00 +0100

    feat: Validator for create short url request

--- app/Http/Controllers/Api/Site/CreateShortUrlController.php
+++ app/Http/Controllers/Api/Site/CreateShortUrlController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Api\Site;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ShortUrlResource;
-use App\Http\Resources\UserResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Src\Context\Site\Infrastructure\Controllers\CreateShortUrlController as HACreateShortUrlController;
 
 class CreateShortUrlController extends Controller
@@ -29,7 +29,12 @@ class CreateShortUrlController extends Controller
      */
     public function __invoke(Request $request) : JsonResponse
     {
-        // TODO: Validate
+        $validator = Validator::make($request->all(), [
+            'url' => 'required|url:http,https|max:255'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), 400);
+        }
         $shortUrl = new ShortUrlResource($this->createShortUrlController->__invoke($request));
         return response()->json($shortUrl, 200);
     }
